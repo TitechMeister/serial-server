3394b4ff325cbe52f797c23a5ac58995683dedfa
elnath-tkg <84269936+elnath-tkg@users.noreply.github.com>
2025-07-21 17:00:25 +0900

    バグ修正

--- background/background.py
+++ background/background.py
@@ -37,12 +37,13 @@ class Background:
                     return
                 print(f"Latest data: {data}")
                 parsed_data, parser_name = self.parser_manager.parse_data(data[0])
+                print(f"Parsed data: {parsed_data}, Parser name: {parser_name}")
                 if parser_name is None:
                     print("No parser found for the data.")
                 else:
                     parsed_data["received_time"] = data[1]  # Use the received time from the queue
                     with shared_data.data_lock:
-                        shared_data.data_dict[data[1]] = parsed_data  # Assuming data is a tuple (parsed_data, parser_name)
+                        shared_data.data_dict[parser_name] = parsed_data  # Assuming data is a tuple (parsed_data, parser_name)
             else:
                 threading.Event().wait(0.05)  # Wait for 0.05 seconds before checking again
 
